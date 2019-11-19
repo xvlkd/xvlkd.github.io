fb9e1443f5ce95904e3c57a53237d3dbf0e47f26
xvlkd <velikaib99@hotmail.com>
2019-11-19 09:25:14 +0700

    commit”

---
+++ precache-manifest.d8d3a9202f9f5f384be8b2b0030fefce.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "405e56b54b2d13890a17",
+    "url": "/css/chunk-3ca7b440.826a16d8.css"
+  },
+  {
+    "revision": "d6c0b0aa626106fda0b3",
+    "url": "/css/chunk-c6c2ba42.34fe009c.css"
+  },
+  {
+    "revision": "9d79e1811b0f708b5a0d",
+    "url": "/css/chunk-vendors.a445e52b.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "b5149ab008818b4b2adb5fa4c76d13ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85c2eed5eb9036d4ac55",
+    "url": "/js/about.133f8cd3.js"
+  },
+  {
+    "revision": "58bc64a430d094fa6db1",
+    "url": "/js/app.087cbd84.js"
+  },
+  {
+    "revision": "405e56b54b2d13890a17",
+    "url": "/js/chunk-3ca7b440.a119a9fd.js"
+  },
+  {
+    "revision": "d6c0b0aa626106fda0b3",
+    "url": "/js/chunk-c6c2ba42.fe62a63b.js"
+  },
+  {
+    "revision": "9d79e1811b0f708b5a0d",
+    "url": "/js/chunk-vendors.60e95d54.js"
+  },
+  {
+    "revision": "69b6aecc10d7facf854d4a9916fa0df6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
